38392b01060497e1ffa106e84423a6858a025ed0
Alexey Pechnikov <pechnikov@mobigroup.ru>
2020-06-01 18:22:51 +0700

    Fix script and picture for centering

--- Spectrograms.sh
+++ Spectrograms.sh
@@ -4,14 +4,14 @@ convert \
     "ASI_PRISMA/ASI PRISMA Spectrogram VNIR and SWIR Bands.jpg" \
     "EO1_HYPERION/EO-1 HYPERION Spectrogram VNIR and SWIR Bands.jpg" \
     "COPERNICUS_S3_OLCI/COPERNICUS_S3_OLCI Spectrogram VNIR and SWIR Bands.jpg" \
-    -append -gravity center \
+    -gravity center -append \
     Spectrograms1.jpg
 
 convert \
     "COPERNICUS_S2_SR/COPERNICUS_S2_SR Spectrogram VNIR and SWIR Bands.jpg" \
     "ASTER_AST_L1T_003/ASTER_AST_L1T_003 Spectrogram VNIR and SWIR Bands.jpg" \
     "LANDSAT_LC08_C01_T1_SR/LANDSAT_LC08_C01_T1_SR Spectrogram VNIR and SWIR Bands.jpg" \
-    -append -gravity center \
+    -gravity center -append \
     Spectrograms2.jpg
 
 convert \
@@ -23,7 +23,7 @@ convert \
 convert \
     "ASTER_AST_L1T_003/ASTER_AST_L1T_003 Spectrogram VNIR and SWIR Bands FULL.jpg" \
     "LANDSAT_LC08_C01_T1_SR/LANDSAT_LC08_C01_T1_SR Spectrogram VNIR and SWIR Bands FULL.jpg" \
-    -append -gravity center \
+    -gravity center -append \
     Spectrograms_FULL.jpg
 
 convert \
@@ -31,5 +31,5 @@ convert \
     "ASI_PRISMA/ASI PRISMA VNIR and SWIR Bands.jpg" \
     "EO1_HYPERION/EO-1 HYPERION Spectrogram VNIR and SWIR Bands.jpg" \
     "EO1_HYPERION/EO-1 HYPERION VNIR and SWIR Bands.jpg" \
-    -append -gravity center \
+    -gravity center -append \
     ASI_PRISMA_EO1_HYPERION.jpg
